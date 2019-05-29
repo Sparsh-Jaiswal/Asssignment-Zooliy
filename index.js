e9fe656dd418fc25166a6afab1466250d4103712
Sparsh Jaiswal <sparshjs1998@gmail.com>
2019-05-30 04:37:52 +0530

    Get Request Handled

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const mongoose=require('mongoose');
 var {Asset} = require('./models/Asset');
 
 const url = 'mongodb://localhost:27017';
+
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
+    console.log("Connected correctly to Mongodb server");
     }, (err) => { console.log(err); });
 
 const app=express();
@@ -19,15 +20,14 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-app.post('/transfers',async(req,res) => {
-    //console.log("level1");
+app.post('/transfer',async(req,res) => {
     try{
         console.log(req.body);
         var ass=new Asset({
             Info: req.body.Info ,
             Reciever: req.body.Reciever,
             Sender: req.body.Sender ,
-            timestamp: new Date()});
+            Timestamp: new Date()});
         var doc=await ass.save();
         res.send(doc);
     }
@@ -37,16 +37,18 @@ app.post('/transfers',async(req,res) => {
     }
 });
 
-app.get('/transfers',async(req,res) => {
+app.get('/transfer',async(req,res) => {
     try{
-        res.send("Hello World1");
+        const doc=await Asset.find({});
+        res.send(doc);
     }
     catch(e){
+        console.log(e);
         res.status(400).send();
     }
 });
 
-app.put('/transfers',async(req,res)=> {
+app.put('/transfer',async(req,res)=> {
     try{
         res.send("Put Operation Currently not Available on Transfers");
     }
@@ -55,7 +57,7 @@ app.put('/transfers',async(req,res)=> {
     }
 });
 
-app.delete('/transfers',async(req,res)=> {
+app.delete('/transfer',async(req,res)=> {
     try{
         res.send("Delete Operation Currently not Available on Transfers");
     }
